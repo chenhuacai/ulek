4143c4f7898eef1c5492017ee5eac6ca87b0f2d4
Huacai Chen <chenhc@lemote.com>
2019-06-03 10:16:02 +0800

    USB: XHCI: Fix device lost problems on ETRON controller
    Signed-off-by: Huacai Chen <chenhc@lemote.com>
    Signed-off-by: Liangliang Huang <huangll@lemote.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -257,11 +257,13 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 		xhci->quirks |= XHCI_RESET_ON_RESUME;
 		xhci->quirks |= XHCI_TRUST_TX_LENGTH;
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
+		xhci->quirks |= XHCI_ETRON_HOST;
 	}
 	if (pdev->vendor == PCI_VENDOR_ID_ETRON &&
 			pdev->device == PCI_DEVICE_ID_EJ188) {
 		xhci->quirks |= XHCI_RESET_ON_RESUME;
 		xhci->quirks |= XHCI_BROKEN_STREAMS;
+		xhci->quirks |= XHCI_ETRON_HOST;
 	}
 
 	if (pdev->vendor == PCI_VENDOR_ID_RENESAS &&
